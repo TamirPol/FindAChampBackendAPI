a8a3220d5a0f363a23891898a34251cb2846ea59
Tamir Polyakov <69022802+TamirPol@users.noreply.github.com>
2023-06-03 22:55:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from datetime import date
 
 app = flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/nizcomix/Mushroom Identifier/db/master.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/TamirPol/FindAChampForked/master.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
